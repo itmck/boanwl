8e40c5dbd2fff6663a812b8f1b276ec893bf98b0
junjun <1176953956@qq.com>
2018-07-23 22:41:18 +0800

    运单流水

---
+++ boanwl-manager/boanwl-manager-web/src/main/java/com/boanwl/manager/web/TransController.java
@@ -0,0 +1,12 @@
+package com.boanwl.manager.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @date 2018/7/23 22:37
+ * @since jiangjun
+ */
+@Controller
+public class TransController {
+
+}
