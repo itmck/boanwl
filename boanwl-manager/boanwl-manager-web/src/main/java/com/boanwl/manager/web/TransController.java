b45f61ffad1a4f5c4f0126b88a75211808acda83
junjun <1176953956@qq.com>
2018-07-24 11:34:33 +0800

    测试用

--- boanwl-manager/boanwl-manager-web/src/main/java/com/boanwl/manager/web/TransController.java
+++ boanwl-manager/boanwl-manager-web/src/main/java/com/boanwl/manager/web/TransController.java
@@ -9,4 +9,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TransController {
 
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
 }
