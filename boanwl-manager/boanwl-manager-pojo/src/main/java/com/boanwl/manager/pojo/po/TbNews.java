f1eaa757dc8ff8cdcada170f24db4a1100c91b59
HzBoan <1028151949@qq.com>
2018-07-24 20:26:51 +0800

    新闻增删ganha

--- boanwl-manager/boanwl-manager-pojo/src/main/java/com/boanwl/manager/pojo/po/TbNews.java
+++ boanwl-manager/boanwl-manager-pojo/src/main/java/com/boanwl/manager/pojo/po/TbNews.java
@@ -3,7 +3,7 @@ package com.boanwl.manager.pojo.po;
 import java.util.Date;
 
 public class TbNews {
-    private Integer id;
+    private String id;
 
     private String title;
 
@@ -15,12 +15,12 @@ public class TbNews {
 
     private String content;
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
     }
 
     public String getTitle() {
