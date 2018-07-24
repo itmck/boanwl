407303fafc7120c503d543cc0aa57d538656ebb9
HzBoan <1028151949@qq.com>
2018-07-24 08:44:25 +0800

    SendQueryDTO 更新

--- boanwl-manager/boanwl-manager-pojo/src/main/java/com/boanwl/manager/pojo/dto/SendQueryDTO.java
+++ boanwl-manager/boanwl-manager-pojo/src/main/java/com/boanwl/manager/pojo/dto/SendQueryDTO.java
@@ -7,6 +7,45 @@ package com.boanwl.manager.pojo.dto;
  * Version:V1.0
  */
 public class SendQueryDTO {
+    private Integer page;
+    private Integer limit;
+    private String sid;
+    private String senderName;
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+    public int getOffset() {
+        return (page - 1) * limit;
+    }
 
 
 }
