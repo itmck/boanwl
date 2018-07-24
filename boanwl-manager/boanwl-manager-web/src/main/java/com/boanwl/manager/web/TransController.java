bc313d6a888d8b122f364f0027756d1b59ada635
junjun <1176953956@qq.com>
2018-07-24 12:04:14 +0800

    测试1

--- boanwl-manager/boanwl-manager-web/src/main/java/com/boanwl/manager/web/TransController.java
+++ boanwl-manager/boanwl-manager-web/src/main/java/com/boanwl/manager/web/TransController.java
@@ -11,6 +11,6 @@ public class TransController {
 
     public static void main(String[] args) {
         System.out.println("ok");
-        System.out.println("第二次");
+        System.out.println("第二次++++");
     }
 }
