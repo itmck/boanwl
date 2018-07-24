f1a087b04fbf86e271558e2c5c2c8538bbd1d20f
HzBoan <1028151949@qq.com>
2018-07-24 08:41:39 +0800

    刀 层 新增

--- boanwl-manager/boanwl-manager-dao/src/main/java/com/boanwl/manager/dao/SendMapper.java
+++ boanwl-manager/boanwl-manager-dao/src/main/java/com/boanwl/manager/dao/SendMapper.java
@@ -1,5 +1,10 @@
 package com.boanwl.manager.dao;
 
+import com.boanwl.manager.pojo.dto.SendQueryDTO;
+import com.boanwl.manager.pojo.po.TbSend;
+
+import java.util.List;
+
 /**
  * User: Boan
  * Date: 2018/7/23
@@ -7,4 +12,8 @@ package com.boanwl.manager.dao;
  * Version:V1.0
  */
 public interface SendMapper {
+
+    List<TbSend> listSends(SendQueryDTO sendQueryDTO);
+
+    Long countSends(SendQueryDTO sendQueryDTO);
 }
