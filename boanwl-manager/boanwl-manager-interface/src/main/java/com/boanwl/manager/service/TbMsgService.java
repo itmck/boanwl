a6e657c939cc66d3e57a1b6fcbcf6b4095e5d191
itmck <347451331@qq.com>
2018-07-26 19:02:26 +0800

    新增回复留言功能

--- boanwl-manager/boanwl-manager-interface/src/main/java/com/boanwl/manager/service/TbMsgService.java
+++ boanwl-manager/boanwl-manager-interface/src/main/java/com/boanwl/manager/service/TbMsgService.java
@@ -3,6 +3,7 @@ package com.boanwl.manager.service;
 import com.boanwl.common.dto.ItemDTO;
 import com.boanwl.manager.pojo.dto.PageParam;
 import com.boanwl.manager.pojo.dto.TbQueryDTO;
+import com.boanwl.manager.pojo.po.TbResp;
 import com.boanwl.manager.pojo.vo.TbMsgCustom;
 
 /**
@@ -10,5 +11,9 @@ import com.boanwl.manager.pojo.vo.TbMsgCustom;
  * @ version 1.0
  */
 public interface TbMsgService {
+    //查询
     ItemDTO<TbMsgCustom> getRequestList(PageParam pageParam, TbQueryDTO tbQueryDTO);
+
+    //回复
+    int respMsg(TbResp tbResp);
 }
