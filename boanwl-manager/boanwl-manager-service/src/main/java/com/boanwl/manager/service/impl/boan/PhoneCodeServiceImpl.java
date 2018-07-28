b23574089d32ffb959a28d6e444104399c1510cb
HzBoan <1028151949@qq.com>
2018-07-28 10:05:49 +0800

    短信服务 redis 限制

--- boanwl-manager/boanwl-manager-service/src/main/java/com/boanwl/manager/service/impl/boan/PhoneCodeServiceImpl.java
+++ boanwl-manager/boanwl-manager-service/src/main/java/com/boanwl/manager/service/impl/boan/PhoneCodeServiceImpl.java
@@ -3,9 +3,14 @@ package com.boanwl.manager.service.impl.boan;
 import com.alibaba.dubbo.common.logger.Logger;
 import com.alibaba.dubbo.common.logger.LoggerFactory;
 import com.boan.comcom.utils.BugRobot;
+import com.boan.comcom.utils.PhoneCodeUtils;
 import com.boan.jedis.JedisClient;
 import com.boanwl.manager.service.boan.PhoneCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: Boan
@@ -13,20 +18,91 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Time: 22:34
  * Version:V1.0
  */
+@Service
 public class PhoneCodeServiceImpl implements PhoneCodeService {
+
+    private final boolean really = false;
 
     @Autowired
     private JedisClient jedisClient;
-
     private BugRobot bugRobot = new BugRobot();
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Override
+    public Map<String, Object> sendRegisterCode(String phoneNumber) {
+        return getCode(phoneNumber, true);
+    }
 
     @Override
-    public String sendCode(String ip, String phoneNumber) {
+    public Map<String, Object> sendResetPasswordCode(String phoneNumber) {
+        return getCode(phoneNumber, false);
+    }
 
-        String count = jedisClient.hget(ip, phoneNumber);
+    @Override
+    public boolean checkRegisterCode(String phoneNumber, String code) {
+        return checkCode(phoneNumber, code);
+    }
 
-        return null;
+    @Override
+    public boolean checkResetPasswordCode(String phoneNumber, String code) {
+        return checkCode(phoneNumber, code);
+    }
+
+    public boolean checkCode(String phoneNumber, String code) {
+
+        String codeKey = phoneNumber + "code";
+        if (!jedisClient.exists(codeKey))
+            return false;
+        if (!jedisClient.get(codeKey).equals(code))
+            return false;
+        return true;
+
+    }
+
+    private Map<String, Object> getCode(String phoneNumber, boolean register) {
+
+        String countKey = phoneNumber + "count";
+        String codeKey = phoneNumber + "code";
+
+        Map<String, Object> map = new HashMap<>();
+        if (jedisClient.exists(countKey)) {
+
+            int count = Integer.parseInt(jedisClient.get(countKey));
+            if (count >= 5) {
+
+                map.put("state", "1");
+                map.put("msg", "一个手机一天只能发送5次");
+
+            } else {
+                map.put("code", sendCode(phoneNumber, register));
+                jedisClient.incr(countKey);
+                map.put("state", "0");
+            }
+            return map;
+        } else {
+            map.put("code", sendCode(phoneNumber, true));
+            jedisClient.set(countKey, "1");
+            jedisClient.expire(countKey, 86400);
+            map.put("state", "0");
+            return map;
+        }
+    }
+
+    private String sendCode(String phoneNumber, boolean register) {
+        String code = null, codeKey = phoneNumber + "code";
+        try {
+            if (register) {
+                code = PhoneCodeUtils.sendRegisterCode(phoneNumber, really);
+            } else {
+                code = PhoneCodeUtils.sendResetPasswordCode(phoneNumber, really);
+            }
+        } catch (Exception e) {
+            bugRobot.sendErrorToDD(e, "13567557892");
+            logger.error(e.getMessage(), e);
+            e.printStackTrace();
+        }
+        jedisClient.set(codeKey, code);
+        jedisClient.expire(codeKey, 300);
+        return code;
     }
 }
